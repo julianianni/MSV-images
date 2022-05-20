03fe49c0b7f30abcb434b39b8100fa1200f60093
Julian Ianni <julianianni@gmail.com>
2022-05-20 17:39:15 -0300

    remove test

--- tstest/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const TOTAL_TAKE = 10
-export const PRODUCTS_PER_CATEGORY = 10
-export const CATEGORIES_URL = 'https://egld.community/api'
-
-//export const CATEGORIES_URL = 'https://www.cryptoatlas.io/api'
-//Updating this URL and commented the below updates the whole API Call
